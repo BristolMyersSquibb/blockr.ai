fed41abbb5d87ac38a677eb83bf2468cf1dc9ffb
Antoine Fabri <antoine.fabri@gmail.com>
2025-02-07 18:07:01 +0100

    remove obsolete tests and function

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,3 @@
-#' @noRd
-#' @param x list of datasets
-name_unnamed_datasets <- function(x) {
-  names(x) <- if_else(
-    names2(x) == "",
-    paste0("data", seq_along(x)),
-    names2(x)
-  )
-  x
-}
-
 # Define response types
 type_response <- function() {
   type_object(
