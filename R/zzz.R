a1b43410948812f6ed83ce28f98f482f9446f23e
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-06-05 15:30:37 +0200

    register gt block

--- R/zzz.R
+++ R/zzz.R
@@ -2,19 +2,32 @@ register_ai_blocks <- function() {
   register_blocks(
     c(
       "new_llm_transform_block",
-      "new_llm_plot_block"
+      "new_llm_plot_block",
+      "new_llm_gt_block"
     ),
     name = c(
       "LLM transform block",
-      "LLM plot block"
+      "LLM plot block",
+      "LLM table block"
     ),
     description = c(
-      "Transform data using LLM-generated R code based on natural language questions",
-      "Create plots using LLM-generated R code based on natural language questions"
+      paste(
+        "Transform data using LLM-generated R code based on natural language",
+        "questions"
+      ),
+      paste(
+        "Create plots using LLM-generated R code based on natural language",
+        "questions"
+      ),
+      paste(
+        "Create tables using LLM-generated R code based on natural language",
+        "questions"
+      )
     ),
     category = c(
       "transform",
-      "plot"
+      "plot",
+      "table"
     ),
     package = utils::packageName(),
     overwrite = TRUE
