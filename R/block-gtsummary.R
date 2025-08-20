d7d301b3f903d517e9fcd3a284b034089a8fddb2
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-08-20 17:33:57 +0200

    minor edits for gtsummary

--- R/block-gtsummary.R
+++ R/block-gtsummary.R
@@ -13,14 +13,16 @@ block_ui.llm_gtsummary_block <- function(id, x, ...) {
 
 #' @export
 block_output.llm_gtsummary_block <- function(x, result, session) {
+
   if (inherits(result, "gtsummary")) {
     result <- gtsummary::as_gt(result)
   }
-   gt::render_gt(gt_theme(result, session))
+
+  gt::render_gt(gt_theme(result, session))
 }
 
 
 #' @export
 result_ptype.llm_gtsummary_block_proxy <- function(x) {
-  gt::gt(data.frame())
+  gtsummary::tbl_summary(data.frame(a = 1))
 }
