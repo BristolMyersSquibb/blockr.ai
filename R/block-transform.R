720e27480f5740e67e303b48d0af256e31e77979
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2025-09-09 10:57:50 +0200

    split system prompt code org

--- R/block-transform.R
+++ R/block-transform.R
@@ -8,3 +8,23 @@ new_llm_transform_block <- function(...) {
 result_ptype.llm_transform_block_proxy <- function(x) {
   data.frame()
 }
+
+#' @rdname system_prompt
+#' @export
+system_prompt.llm_transform_block_proxy <- function(x, datasets, ...) {
+
+  paste0(
+    NextMethod(),
+    "\n\n",
+    "Your task is to transform input datasets into a single output dataset.\n",
+    "If possible, use dplyr for data transformations.\n",
+    "Use the base R pipe and not the magrittr pipe to make nested function ",
+    "calls more readable.\n\n",
+    "Example of good code you might write:\n",
+    "data |>\n",
+    "  dplyr::group_by(category) |>\n",
+    "  dplyr::summarize(mean_value = mean(value))\n\n",
+    "Important: make sure that your code always returns a transformed ",
+    "data.frame.\n"
+  )
+}
