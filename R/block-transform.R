8fbb9d6ae9f550a0198dc51c3d2795a2b61e099d
Christoph Sax <christoph.sax@gmail.com>
2025-11-26 20:13:07 +0100

    improve prompting to avoid %>% and prefix less n()

--- R/block-transform.R
+++ R/block-transform.R
@@ -17,14 +17,18 @@ system_prompt.llm_transform_block_proxy <- function(x, datasets, ...) {
     NextMethod(),
     "\n\n",
     "Your task is to transform input datasets into a single output dataset.\n",
-    "If possible, use dplyr for data transformations.\n",
-    "Use the base R pipe and not the magrittr pipe to make nested function ",
-    "calls more readable.\n\n",
-    "Example of good code you might write:\n",
+    "If possible, use dplyr for data transformations.\n\n",
+    "IMPORTANT SYNTAX RULES:\n",
+    "- Use the base R pipe |> and NEVER use %>% (magrittr pipe is not available)\n",
+    "- Always use namespace prefixes like dplyr::n(), dplyr::filter(), dplyr::mutate(), etc.\n\n",
+    "Example of good code:\n",
     "data |>\n",
     "  dplyr::group_by(category) |>\n",
-    "  dplyr::summarize(mean_value = mean(value))\n\n",
+    "  dplyr::summarize(\n",
+    "    count = dplyr::n(),\n",
+    "    mean_value = mean(value)\n",
+    "  )\n\n",
     "Important: make sure that your code always returns a transformed ",
-    "data.frame.\n"
+    "data.frame or tibble.\n"
   )
 }
